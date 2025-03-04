60037d777c77e2560f6776c44cbec39612d6da55
deisesalless <deisesalles18@gmail.com>
2025-03-04 15:49:04 -0300

    adicionado comentario sinalizado o Decorator Partten

--- src/main/java/Exercicio1_Caractere.java
+++ src/main/java/Exercicio1_Caractere.java
@@ -5,18 +5,17 @@ public class Exercicio1_Caractere {
     public static void receberTecladoImprimirConsole() throws IOException {
 
 //        Código separado e mais mastigado:
-
+//        ----------------------------------------------------
 //        InputStream sys = System.in;
 //        InputStreamReader input = new InputStreamReader(sys);
 //        BufferedReader br = new BufferedReader(input);
-
+//        ----------------------------------------------------
 //        OutputStream sout = System.out;
 //        OutputStreamWriter out = new OutputStreamWriter(sout);
 //        BufferedWriter bw = new BufferedWriter(out);
-
 //        ----------------------------------------------------
 
-
+//        Forma mais utilizada: Decorator Partten
         System.out.println("Recomende 3 filmes: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
